08ca896546ab725f66e1f0740402806524cb25fe
capecodder90 <scushing90@comcast.net>
2014-10-07 11:16:58 -0400

    Expression Worksheet: Dog Years
    Created a variable for Sparky’s age, a variable for human to dog years,
    and a variable for the result. In the dogYrs variable I created an
    expression in which I concatenated the required strings and multiplied
    Sparky’s human years by the human-to-dog years. I then used the
    console.log function to print the concatenated string in the dogYrs
    variable to the console.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,13 @@
+/*
+Sarah A. Cushing
+Tuesday, October 7, 2014
+Expressions Worksheet
+ */
+
+// Problem: Dog Years
+var humanYrs = 12; // Sparky's age in human years
+var humanToDog = 7; // 1 human year is 7 dog years
+var dogYrs = "Sparky is " + humanYrs + " human years old which is " + humanYrs * humanToDog + " in dog years."; // result container, calculates humanYrs, which is 12 years, times humanToDog, which is 7 years, and concatenates the strings in the expression.
+
+console.log(dogYrs); // displays the result of the calculations and concatenation of the variable dogYrs in the console of the wbe browser
+
