172a94c0f1563760557bb2529f96e24085a0fd9b
capecodder90 <scushing90@comcast.net>
2014-10-07 18:47:33 -0400

    Add comments to pgCont
    Added comments to the pgCont variable to describe what it does

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,4 +6,6 @@
 
 // Calculate the percentage for the width and gutter (margin) of a responsive two-column web page layout.
 
-var pgCont = prompt("What is the width of the page container in pixels?");
+// Determine the width (in pixels) of the page container.
+var pgCont = prompt("What is the width of the page container in pixels?"); // prompt the user to enter the page container width and store the response
+
