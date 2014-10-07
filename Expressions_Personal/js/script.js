4a495b13529f53bc40b976fd78c5be244d7c6714
capecodder90 <scushing90@comcast.net>
2014-10-07 16:46:53 -0400

    Edit comments
    Edit some spelling and grammatical errors in the comments. Make the
    comments format more consistent.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -14,28 +14,28 @@ carName[2] = prompt("What is the name of your third car?"); // asks the user to 
 
 // Have the user input the miles per gallon of each of the three cars
 var mpg = new Array(3); // used to store the miles per gallon of each of the three cars
-mpg[0] = prompt("How many miles per gallon does the " + carName[0] + " get?"); // asks the user to input the miles per gallon for car number 1
-mpg[1] = prompt("How many miles per gallon does the " + carName[1] + " get?"); // asks the user to input the miles per gallon for car number 2
-mpg[2] = prompt("How many miles per gallon does the " + carName[2] + " get?"); // asks the user to input the miles per gallon for car number 3
+mpg[0] = prompt("How many miles per gallon does the " + carName[0] + " get?"); // asks the user to input the miles per gallon for car number 1.
+mpg[1] = prompt("How many miles per gallon does the " + carName[1] + " get?"); // asks the user to input the miles per gallon for car number 2.
+mpg[2] = prompt("How many miles per gallon does the " + carName[2] + " get?"); // asks the user to input the miles per gallon for car number 3.
 
-// Provide the price of gas, and ask the user to input the amount of money they have
-var gasPrice = 3.50; // the price of gas
-var yourMoney = prompt("How much money, to the nearest whole dollar, do you have in your pocket for gas?"); // asks the user to input the amount of money they have in their pocket
+// Provide the price of gas, and ask the user to input the amount of money they have avilable.
+var gasPrice = 3.50; // the price of gas.
+var yourMoney = prompt("How much money, to the nearest whole dollar, do you have in your pocket for gas?"); // asks the user to input the amount of money they have in their pocket.
 
-// Calculate the amount of gas they can purchase, in gallons
-var numGallons = yourMoney / gasPrice; // divide the amount of money available for gas by the price of the gas
+// Calculate the amount of gas they can purchase, in gallons.
+var numGallons = yourMoney / gasPrice; // divide the amount of money available for gas by the price of the gas.
 
-// print the number of gallons that they can buy to the console
+// Print the number of gallons that they can buy to the console.
 console.log("You can afford " + numGallons + " gallons of gas.");
 
-// calculate the number of miles each car can go on the amount of gas that can be purchased
-var numMiles = new Array(3); // used to store the number of miles for each car
-numMiles[0] = mpg[0] * numGallons; // calculates the number of miles for the first car by multiplying the miles per gallon by the number of gallons purchased
-numMiles[1] = mpg[1] * numGallons; // calculates the number of miles for the second car by multiplying the miles per gallon by the number of gallons purchased
-numMiles[2] = mpg[2] * numGallons; // calculates the number of miles for the third car by multiplying the miles per gallon by the number of gallons purchased
+// Calculate the number of miles each car can go on the amount of gas that can be purchased.
+var numMiles = new Array(3); // used to store the number of miles for each car.
+numMiles[0] = mpg[0] * numGallons; // calculates the number of miles for the first car by multiplying the miles per gallon by the number of gallons purchased.
+numMiles[1] = mpg[1] * numGallons; // calculates the number of miles for the second car by multiplying the miles per gallon by the number of gallons purchased.
+numMiles[2] = mpg[2] * numGallons; // calculates the number of miles for the third car by multiplying the miles per gallon by the number of gallons purchased.
 
-// Print the number of miles each car can drive to the consoles
-console.log("You will be able to drive " + numMiles[0] + " miles with the " + carName[0] + ", " + numMiles[1] + " miles with the " + carName[1] + ", and " + numMiles[2] + " miles with the " + carName[2] + "."); // concatenates the strings and pulls in the stored data for each array element
+// Print the number of miles each car can drive to the console.
+console.log("You will be able to drive " + numMiles[0] + " miles with the " + carName[0] + ", " + numMiles[1] + " miles with the " + carName[1] + ", and " + numMiles[2] + " miles with the " + carName[2] + "."); // concatenates the strings and pulls in the stored data for each array element.
 
 var milesNeeded = prompt("How many miles do you need to drive?");
 
