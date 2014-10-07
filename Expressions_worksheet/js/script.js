c86226fc6c630d105ca82990e5208778e5bc24d9
capecodder90 <scushing90@comcast.net>
2014-10-07 11:22:32 -0400

    Expressions Worksheet: Edits and Slice of Pie
    Made some spelling corrections on the first problem. Wrote out the
    variables for the second problem.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -7,7 +7,12 @@ Expressions Worksheet
 // Problem: Dog Years
 var humanYrs = 12; // Sparky's age in human years
 var humanToDog = 7; // 1 human year is 7 dog years
-var dogYrs = "Sparky is " + humanYrs + " human years old which is " + humanYrs * humanToDog + " in dog years."; // result container, calculates humanYrs, which is 12 years, times humanToDog, which is 7 years, and concatenates the strings in the expression.
+var dogYrs = "Sparky is " + humanYrs + " human years old which is " + humanYrs * humanToDog + " in dog years."; // result variable, calculates humanYrs, which is 12 years, times humanToDog, which is 7 years, and concatenates the strings in the expression.
 
-console.log(dogYrs); // displays the result of the calculations and concatenation of the variable dogYrs in the console of the wbe browser
+console.log(dogYrs); // displays the result of the calculations and concatenation of the variable dogYrs in the console of the web browser
 
+// Problem: Slice of Pie Part I
+var slicePerPizza = 10; // the number of slices per pizza
+var partyGoers = 38; // the number of party goers
+var numPizzas = 12; // the number of pizzas ordered
+var numSlices; // the result variable
