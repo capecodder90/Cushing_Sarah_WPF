348c5f8e066087ecbde463d7fe931feb1391f504
capecodder90 <scushing90@comcast.net>
2014-10-07 18:49:55 -0400

    Added comments to firCol
    Added comments to describe what is being stored in the firCol variable

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,4 +9,5 @@
 // Determine the width (in pixels) of the page container.
 var pgCont = prompt("What is the width of the page container in pixels?"); // prompt the user to enter the page container width and store the response
 
-var firCol = prompt("What is the width of the first column in pixels?");
+// Determine the width of the first column (in pixels).
+var firCol = prompt("What is the width of the first column in pixels?"); // prompt the user to enter the first column width and store the response
