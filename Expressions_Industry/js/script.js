c3feba497b73302278a08a30bd0a8df4e70a6a27
capecodder90 <scushing90@comcast.net>
2014-10-07 18:52:00 -0400

    Add Comments to secCol
    Added comments to describe what is happening in the secCol variable

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,9 +7,10 @@
 // Calculate the percentage for the width and gutter (margin) of a responsive two-column web page layout.
 
 // Determine the width (in pixels) of the page container.
-var pgCont = prompt("What is the width of the page container in pixels?"); // prompt the user to enter the page container width and store the response
+var pgCont = prompt("What is the width of the page container in pixels?"); // prompts the user to enter the page container width and stores the response
 
 // Determine the width of the first column (in pixels).
-var firCol = prompt("What is the width of the first column in pixels?"); // prompt the user to enter the first column width and store the response
+var firCol = prompt("What is the width of the first column in pixels?"); // prompts the user to enter the first column width and stores the response
 
-var secCol = prompt("What is the width of the second column in pixels?");
+// Determine the width of the second column (in pixels).
+var secCol = prompt("What is the width of the second column in pixels?"); // prompts the user to enter the second column width and stores the response
