e6dbbc07c5c6c86a793c7b77baddec8c9ea737c3
capecodder90 <scushing90@comcast.net>
2014-10-07 09:55:53 -0400

    Screencast: Example Expressions
    Wrote an expression using my year born, the current year, and
    subtraction to determine my current age, and then write that result to
    the console.

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,6 +1,5 @@
-// Expressions
+// Expressions - Age example
 
-var a = 2; // sets up our variable a and defines it with 2
-var b;
-b = a + 3; // add 3 to a
-console.log(b);
+var yearBorn = 1990;
+var age = 2014 - yearBorn;
+console.log(age);
