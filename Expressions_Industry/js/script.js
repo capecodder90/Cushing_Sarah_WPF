44284690f2bed6fd4635f260a0da302a88f50416
capecodder90 <scushing90@comcast.net>
2014-10-07 19:00:01 -0400

    Added expression for margin size
    declared a new variable with an expression that calculates the
    percentage for the size of the margin

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -23,3 +23,5 @@ var firColWidth = pgCont / firCol * 100; // divides the page container width by 
 
 // Calculate the percentage for the width of the second column.
 var secColWidth = pgCont / secCol * 100; // divides the page container width by the width of the second column, then multiplies the decimal by 100 to get the percent
+
+var margin = pgCont / marRight;
