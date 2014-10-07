6f5347a99d13196a0b4085efd000d2f2278ee1f6
capecodder90 <scushing90@comcast.net>
2014-10-07 13:52:23 -0400

    Prompts and Alerts
    Created some prompts and alerts. Used the input from the prompts in an
    expression.

---
+++ PromptAndAlert/js/script.js
@@ -0,0 +1,12 @@
+// Prompt and Alert
+
+var width = prompt("We are calculating the are of a rectangle. \nPlease enter the width:");
+var height = prompt("Please enter the height:");
+var area = width * height; //calculates with that ino
+//console.log(area); // print it out to the console
+var result = "The area of your rectangle is " + area + " sq feet.";
+alert(result);
+
+
+
+
