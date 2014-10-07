40faeb32a99b19d00f155c740fae1927c7eb85bd
capecodder90 <scushing90@comcast.net>
2014-10-07 15:50:06 -0400

    Working on the criteria
    Writing out everything that will be needed to meet the criteria.
    Including the variables and the problem.

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,14 @@
+/*
+Sarah A. Cushing
+Tuesday, October 7, 2014
+Expressions Assignment: Personal
+ */
+
+// You have three different cars available to you. Enter what the name of the vehicle is and the miles-per-gallon. Determine how much gas you can put into each car's gas tank with the amount of money you have in your pocket. How many miles will you be able to drive with this much gas in each of the cars. How many miles do you need to drive? Determine which vehicle would get you the closest to your destination using the least fuel efficient car possible. Assume all three car's gas tanks are empty to start.
+
+var tankSize = prompt("What is the size (in gallons) of your gas tank?");
+var gasPrice = prompt("What is the current price of gas per gallon?");
+var yourMoney = prompt("How much money do go you for gas?");
+var mpg = new Array[12, 34, 40];
+
+var amtOfGas =
