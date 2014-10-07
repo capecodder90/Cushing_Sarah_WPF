f37caacc0572c4855dc6d8ebbbe2a00b9eaf103f
capecodder90 <scushing90@comcast.net>
2014-10-07 18:45:03 -0400

    The Problem
    Write out the problem

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,5 @@
  Tuesday, October 7, 2014
  Expressions Assignment: Industry Related
  */
+
+// Calculate the percentage for the width and gutter (margin) of a responsive two-column web page layout.
