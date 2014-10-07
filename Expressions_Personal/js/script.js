f495e4c27e86f01ef2af2238500129270482255e
capecodder90 <scushing90@comcast.net>
2014-10-07 15:52:41 -0400

    Add variables for car names
    Added the variables with prompts for the three different car names. Add
    comments.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,7 +6,10 @@ Expressions Assignment: Personal
 
 // You have three different cars available to you. Enter what the name of the vehicle is and the miles-per-gallon. Determine how much gas you can put into each car's gas tank with the amount of money you have in your pocket. How many miles will you be able to drive with this much gas in each of the cars. How many miles do you need to drive? Determine which vehicle would get you the closest to your destination using the least fuel efficient car possible. Assume all three car's gas tanks are empty to start.
 
-var tankSize = prompt("What is the size (in gallons) of your gas tank?");
+var carName1 = prompt("What is the name of your first car?"); // asks the user to input the name of the first car.
+var carName2 = prompt("What is the name of your second car?"); // asks the user to input the name of the second car.
+var carName3 = prompt("What is the name of your third car?"); // asks the user to input the name of the third car.
+var tankSize = prompt("What is the size (in gallons) of your gas tank?"); // asks the user to input the size in gallons of each gas tank
 var gasPrice = prompt("What is the current price of gas per gallon?");
 var yourMoney = prompt("How much money do go you for gas?");
 var mpg = new Array[12, 34, 40];
