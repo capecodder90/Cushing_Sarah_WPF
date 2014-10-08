4e3217f1c4e7d140e8f5b30a62ec1ba1786ba735
capecodder90 <scushing90@comcast.net>
2014-10-08 06:42:29 -0400

    Added the problem
    Wrote out the problem

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,3 +3,5 @@ Sarah A. Cushing
 Wednesday, October 8, 2014
 Expressions Assignment: Wacky
  */
+
+// Determine how many gallons of milk and chocolate syrup you will need to fill your rectangular bathtub with chocolate milk. Figure it out for your two favorite chocolate syrups. Change the number of tbsp. of syrup to make it more or less chocolate-y.
