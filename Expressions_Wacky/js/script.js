c2986acf69ea8a75fc797f0841f7178fd4e7624a
capecodder90 <scushing90@comcast.net>
2014-10-08 06:55:01 -0400

    Declared/defined volume
    Created the variable volume, added the formula for volume as an
    expression

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,6 +10,11 @@ Expressions Assignment: Wacky
 var syrup1 = prompt("What is the brand of your favorite chocolate syrup?"); // declares syrup1 and uses a prompt to ask the user to input the name of their favorite syrup; stores the input
 var syrup2 = prompt("What is the brand of your second favorite chocolate syrup?"); // declares syrup2 and uses a prompt to ask the user to input the name of their second favorite syrup; stores the input
 
-var width = prompt("What is the length of the inside of your bathtub in inches");
-var length = prompt("What is the width of the inside of your bathtub in inches");
-var height = prompt("What is the height of the inside of your bathtub in inches");
+// Ask the user for the dimensions of the inside of their bathtub
+var width = prompt("What is the length of the inside of your bathtub in inches"); // declares width and uses a prompt to ask the user to input the width of the tub in inches, stores the input
+var length = prompt("What is the width of the inside of your bathtub in inches"); // declares length and uses a prompt to ask the user to input the length of the tub in inches, stores the input
+var height = prompt("What is the height of the inside of your bathtub in inches"); // declares height and uses a prompt to ask the user to input the height of the tub in inches, stores the input
+
+// Calculate the volume of the bathtub
+var volume = width * length * height; // declares volume, multiplies the width times the length times the height, stores the calculation
+
