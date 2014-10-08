0aaa459308b69f0eca8893f1a995d5c2076b1cbb
capecodder90 <scushing90@comcast.net>
2014-10-08 07:31:06 -0400

    Add comments/fix comments
    Added comments to describe what the final console.log function is
    doing, fixed up some of the other comments

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -21,22 +21,23 @@ var height = prompt("What is the height of the inside of your bathtub in inches"
 var volume = width * length * height; // declares volume, multiplies the width times the length times the height, stores the calculation
 
 // Print result of formula to the console.
-console.log("The volume of your bathtub is " + volume + " cubed inches.");// Prints the stored data in the volume variable to the console
+console.log("The volume of your bathtub is " + volume + " cubed inches.");// Concatenates the string, and prints the stored data in the volume variable to the console
 
 // Convert inches cubed to US gallons liquid: inches cubed divided by 231.
 var numGallons = parseInt(volume / 231); // declares numGallons, calculates the volume divided by 231, stores the answer
 
 // Print result of formula to the console.
-console.log("Your bathtub holds approximately " + numGallons + " US gallons liquid."); // Prints the stored data in the numGallons variable to the console
+console.log("Your bathtub holds approximately " + numGallons + " US gallons liquid."); // Concatenates the string, and prints the stored data in the numGallons variable to the console
 
 // How many tbsp of syrup per gallon, based on the figure in tbsp per 8 oz?
 var tbsp = 2; // declares tbsp and defines it as 2
 var tbspPerGal = tbsp * 128 / 8; // declares tbspPerGal, takes the value in tbsp, multiples it by 128 (the number of oz in a gallon), and then divides that result by 8 oz (the number of oz held by most drinking glasses)
 
 // Print result of the formula to the console.
-console.log("You will need " + tbspPerGal + " tbsp. per US gallon of milk."); // Prints the stored data in the tbspPerGal variable to the console
+console.log("You will need " + tbspPerGal + " tbsp. per US gallon of milk."); // Concatenates the string, and prints the stored data in the tbspPerGal variable to the console
 
 // Calculate how many tbsp. are need for the number of gallons of milk we are putting in the bathtub.
 var numSyrup = tbspPerGal * numGallons; // declares numSyrup, multiplies the number of tablespoons per gallon by the number of gallons of milk
 
-console.log("You will need " + numGallons + " US gallons liquid of milk. You will also need " + numSyrup + " tablespoons of " + syrupBrand[0] + " Chocolate Syrup, or " + syrupBrand[1] + " Chocolate Syrup.");
+// Print result to the console.
+console.log("You will need " + numGallons + " US gallons liquid of milk. You will also need " + numSyrup + " tablespoons of " + syrupBrand[0] + " Chocolate Syrup, or " + syrupBrand[1] + " Chocolate Syrup. Have fun!"); // concatenates the strings, and prints the result to the console
