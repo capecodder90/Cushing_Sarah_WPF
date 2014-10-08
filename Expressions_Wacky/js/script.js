5c1b4b1ac12b45865fe0e3156e833a39896600c9
capecodder90 <scushing90@comcast.net>
2014-10-08 06:47:18 -0400

    Comment syrup1 and syrup2
    Added comments to describe what these variables are doing/what they
    store

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,5 +6,6 @@ Expressions Assignment: Wacky
 
 // Determine how many gallons of milk and chocolate syrup you will need to fill your rectangular bathtub with chocolate milk. Figure it out for your two favorite chocolate syrups. Change the number of tbsp. of syrup to make it more or less chocolate-y.
 
-var syrup1 = prompt("What is the brand of your favorite chocolate syrup?");
-var syrup2 = prompt("What is the brand of your second favorite chocolate syrup?");
+// Ask the user their favorite brands of chocolate syrup.
+var syrup1 = prompt("What is the brand of your favorite chocolate syrup?"); // declares syrup1 and uses a prompt to ask the user to input the name of their favorite syrup; stores the input
+var syrup2 = prompt("What is the brand of your second favorite chocolate syrup?"); // declares syrup2 and uses a prompt to ask the user to input the name of their second favorite syrup; stores the input
