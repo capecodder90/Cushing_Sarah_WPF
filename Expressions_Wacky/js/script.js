c3492e87fdb99995eda17e37fee085b5c7860a7a
capecodder90 <scushing90@comcast.net>
2014-10-08 06:44:49 -0400

    Added syrup variables
    Declared syrup1 and syrup2 and defined both variables with a prompt
    that requests the name of each syrup brand

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,3 +5,6 @@ Expressions Assignment: Wacky
  */
 
 // Determine how many gallons of milk and chocolate syrup you will need to fill your rectangular bathtub with chocolate milk. Figure it out for your two favorite chocolate syrups. Change the number of tbsp. of syrup to make it more or less chocolate-y.
+
+var syrup1 = prompt("What is the brand of your favorite chocolate syrup?");
+var syrup2 = prompt("What is the brand of your second favorite chocolate syrup?");
