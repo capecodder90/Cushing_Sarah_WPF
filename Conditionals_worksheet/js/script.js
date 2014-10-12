12e1389179cd142bf9f3679ea33bb97311fc6c4c
capecodder90 <scushing90@comcast.net>
2014-10-12 15:20:55 -0400

    Tire Pressure I: Declare givens, write if
    Declared the given (tire pressure) as an array. Wrote the if statement
    to check the pressure in both the front set and back set of tires.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -44,3 +44,11 @@ if((userName === corUserName) && (userPass === corUserPass)){
 }
 
 // Group 3: Multiple Conditions: Tire Pressure I
+
+// Check if the front two tires have the same air pressure and if the rear two tires have the same air pressure
+
+var tirePressure = new Array[80,80,81,81]; // declares the variable tirePressure, assigns an array that store the tire pressure of each tire. Index 0 = front driver's side, index 1 = front passenger's side, index 2 = rear driver's side, index 3 = rear passenger's side
+
+if((tirePressure[0] === tirePressure[1]) && (tirePressure[2] === tirePressure[3])){
+    console.log("The tires pass spec!");
+}
