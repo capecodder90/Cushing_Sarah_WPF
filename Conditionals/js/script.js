c47b02d542e0eaa43bc755cdf4f1ded67cf076cc
capecodder90 <scushing90@comcast.net>
2014-10-12 12:19:08 -0400

    Screencast: Ternary Operator cont.
    Used the ternary operator to decide which book the child would be given
    and assigned it as the value of the variable book

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,12 +1,8 @@
 // Conditionals Logic - Ternary Operators
 
-var gpa = 48;
+var age = 6;
+var book;
 
-//if the gpa is over the min 2.0 score, the student can graduate
-/*if(gpa > 2.0){
-    console.log("You can graduate!");
-}else{
-    console.log("GPA is too low!");
-}*/
-
-(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+// if the child is under 10, they get Green Eggs an Ham, otherwise they get The Time Machine
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
