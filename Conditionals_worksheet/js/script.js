d2d1699ed1693546196fa38bc6bb357d2ef0536f
capecodder90 <scushing90@comcast.net>
2014-10-12 14:33:04 -0400

    Last chance for gas: Setup givens
    Declared and defined the givens for the last chance for gas problem

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -6,6 +6,10 @@ Assignment: Conditionals Worksheet
 
 // Group 1: Expressions with Conditionals: Last Chance for Gas!
 
+var mpg = 12; // declares mpg, defines as 12 miles per gallon
+var amtOfGas = 25; // declares amtOfGas, defines as 25% of the tank remaining
+var tankCapacity = 30; // declares tankCapacity, defines the tank capacity as 30 gallons
+
 
 // Group 2: Multiple Results: Check the Login
 
