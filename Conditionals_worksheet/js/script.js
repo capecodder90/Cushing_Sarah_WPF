cc53a9f5650633fc1b08900dd70ac3c6e6723703
capecodder90 <scushing90@comcast.net>
2014-10-12 14:46:20 -0400

    Last Chance for Gas: Create if else statement
    Created an if statement using a relational operator in the condition,
    and an else statement

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -7,8 +7,20 @@ Assignment: Conditionals Worksheet
 // Group 1: Expressions with Conditionals: Last Chance for Gas!
 
 var mpg = 12; // declares mpg, defines as 12 miles per gallon
-var amtOfGas = 25; // declares amtOfGas, defines as 25% of the tank remaining
+var amtOfGas = 99; // declares amtOfGas, defines as 25% of the tank remaining
 var tankCapacity = 30; // declares tankCapacity, defines the tank capacity as 30 gallons
+
+// Calculate whether the vehicle can drive another 200 miles on the amount of gas left in the tank.
+
+amtOfGas = amtOfGas/100; // change the percentage to a decimal by diving the percentage by 100, store the new value
+
+// if the percentage of gas left in the tank times the tank's capacity, divided by the number of miles per gallon is greater than or equal to 200 miles, print "Yes you can make it" message to the console
+if(amtOfGas * tankCapacity * mpg >= 200){
+    console.log("Yes, you can make it without stopping for gas!");
+}else{
+    // otherwise, print the "You only have X number of miles, get gas now!" message to the console
+    console.log("You only have " + amtOfGas * tankCapacity + " gallons of gas in your tank, better get gas now while you can!")
+}
 
 
 // Group 2: Multiple Results: Check the Login
