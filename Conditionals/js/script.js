6d5494f71a6885d5f70ad7c7e394e78ed7b78428
capecodder90 <scushing90@comcast.net>
2014-10-12 12:03:29 -0400

    Screencast: Logical Operators OR
    Changed the and (&&) operator to the or (||) operator, and added a
    Boolean value

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,11 +1,11 @@
-// Conditionals Logic - Logical Operator
+// Conditionals Logic - Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var payCheck = 200;
+var wonLottery = true;
 
-// if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && payCheck > 300){
+// if the price of the phone is less than our budget OR we won the lottery
+if(iPhonePrice < budget || wonLottery === true){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!!");
