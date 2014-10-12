1c0810ad573cee8dbcae7eab27f0678d8cd4a145
capecodder90 <scushing90@comcast.net>
2014-10-12 15:08:16 -0400

    Check the Login: Added else if and else
    Added an else if statement to check if the username matches, and an
    else statement to handle it if the user is not found. Add comments

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -28,13 +28,19 @@ if(amtOfGas * tankCapacity * mpg >= 200){
 // Check if the username and password match; if not check if the username if correct, if that is correct, check if the password is correct.
 
 var userName = String(prompt("What is your username?")); // declares userName, prompts the user to enter the user name, converts any input to a string and stores the value
-var userPass = String(prompt("What is you password?")); // declares userPass, prompts the user to enter the password, converts any input to a string and stores the value
+var userPass = String(prompt("What is your password?")); // declares userPass, prompts the user to enter the password, converts any input to a string and stores the value
 var corUserName = "capecodder90"; // declares corUserName, defines the correct user name
 var corUserPass = "1234cc"; // declares corUserPass, defines the correct password
 
 // Check if the entered username is the same as the correct user name AND check if the entered password is the same as the correct password
 if((userName === corUserName) && (userPass === corUserPass)){
     console.log("Welcome, " + userName + "!");
+}else if(userName === corUserName){
+    // If the first if is false, continue on to check if the user name entered is correct, if so, then the password is incorrect, so print that message to the console
+    console.log("Password does not match our records.");
+}else{
+    // Otherwise, the user name entered was incorrect, so print that message to the console
+    console.log("User not found. Try again.");
 }
 
 // Group 3: Multiple Conditions: Tire Pressure I
