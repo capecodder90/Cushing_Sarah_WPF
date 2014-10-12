d0ebf76a08b7293f6f54aabcf47d9e8efba93650
capecodder90 <scushing90@comcast.net>
2014-10-12 14:29:34 -0400

    Chose problems to solve
    Wrote out which problems I wanted to solve in comments

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -5,3 +5,9 @@ Assignment: Conditionals Worksheet
  */
 
 // Group 1: Expressions with Conditionals: Last Chance for Gas!
+
+
+// Group 2: Multiple Results: Check the Login
+
+
+// Group 3: Multiple Conditions: Tire Pressure I
