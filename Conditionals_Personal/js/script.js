ca8a19df9551d12ecb3a2469b0bfb0d19c5bee8c
capecodder90 <scushing90@comcast.net>
2014-10-13 16:06:48 -0400

    Create variables for constants
    Created variables to hold the constants and switched them out

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,14 +8,21 @@ Conditionals Assignment: Personal
 
 var weather = String(prompt("Is it raining, snowing, or sunny outside?"));
 var temp;
+var minTemp = 25;
+var sunCap = "Sunny";
+var sunLow = "sunny";
+var goLandscape;
 
 if(weather === ""){
     console.log("Please type in a valid response: raining, snowing, or sunny.");
-}else if(weather === "sunny" || weather === "Sunny"){
+}else if(weather === sunCap || weather === sunLow){
     temp = Number(prompt("What is the temperature outside (in F\xB0)?"));
     if(temp == false){
         console.log("Please enter a valid number.");
-    }else if(temp >= 25){
-        console.log("I am going landscaping today!");
+    }else if(temp >= minTemp){
+        landscape = "Its " + temp + " outside. I am going landscaping today!"
+        console.log(goLandscape);
     }
+}else{
+    console.log("I am going to sleep in today.");
 }
