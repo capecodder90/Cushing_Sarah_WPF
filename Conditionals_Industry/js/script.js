b9a7bf53167cf27119efa871f524c38e0c444272
capecodder90 <scushing90@comcast.net>
2014-10-13 18:18:48 -0400

    Fixed 0 balance validation problem
    Fixed the validation problem when 0 is entered as a balance

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,10 +6,14 @@ Conditionals Assignment: Industry-based
 
 // If the balance on the client’s account is paid off, I will start on their next project. Otherwise, I will send them a statement of their account.
 
-var accounts = new Array("Smith","Jones","Hamblin"); // declares accounts, creates an array of the account names, stores the values
-var balanceToStart = 0; // declares balanceToStart, defines it as $0
-var accountName = String(prompt("What is the last name on the account?")); // declares accountName, prompts the user to input the last name on their account
-var accountBalance; // declares accountBalance
-var recentPmts; // declares recentPmts
+var balanceToStart = "0"; // declares balanceToStart, defines it as $0
+var accountBalance = prompt("What is the balance on your last statement?"); // declares accountBalance, prompts the user to input the account balance listed on their last account statement
 var startProject; // declares startProject
 var sendStatement; // declares sendStatement
+
+if(accountBalance == false){
+    if(accountBalance != balanceToStart){
+        console.log("Sorry, but that is not a valid number.");
+    }else if(accountBalance >= Number(balanceToStart)){
+  }
+}
