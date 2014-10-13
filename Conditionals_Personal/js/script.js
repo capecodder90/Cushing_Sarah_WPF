cbb08818bf75184d7e64b278083c631d5e2ed355
capecodder90 <scushing90@comcast.net>
2014-10-13 19:19:04 -0400

    Changed problem
    Realized the problem I wrote out was a lot like the one in the videos,
    changed to use day of the week and the temperature

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,29 +4,30 @@ Monday, October 13, 2014
 Conditionals Assignment: Personal
  */
 
-// If the weather outside is sunny, and the temperature is above 25 degrees Fahrenheit I will go landscaping. Otherwise, I will stay in my bed and sleep in.
+// If it is Friday, and the temperature is above 25 degrees Fahrenheit I will go landscaping. Otherwise, I will stay in my bed and sleep in.
 
-var weather = String(prompt("Is it raining, snowing, or sunny outside?")); // declares weather, prompts the user to input a weather type, stores the response as a string
+var dayOfWeek = String(prompt("What day of the week is it?")); // declares dayOfWeek, prompts the user to input a day, stores the response as a string
 var temp; // declares temp
 var minTemp = 25; // declares minTemp, defines it as 25
-var sunCap = "Sunny"; // declares sunCap, defines it as Sunny, with an uppercase s
-var sunLow = "sunny"; // declares sunLow, defines it as sunny, with a lowercase s
+var friCap = "Friday"; // declares friCap, defines it as Friday, with an uppercase f
+var friLow = "friday"; // declares friLow, defines it as friday, with a lowercase f
 var goLandscape; // declares goLandscape
 var sleepIn = "I am going to sleep in today."; // declares sleepIn, defines it with a string
+var badEntry = "That is not a valid entry." // declares badEntry, defines it as the not a valid entry string
 
-// if the user didn't input anything into the weather prompt, ask them to enter something in the console
-if(weather === ""){
-    console.log("Please type in a valid response: raining, snowing, or sunny.");
-    // if they entered Sunny with a capital S OR sunny with a lowercase s, prompt them to enter the temperature, store it as a number in the temp variable
-}else if(weather === sunCap || weather === sunLow){
+// if the user didn't input anything into the day of the week prompt, ask them to enter something in the console
+if(dayOfWeek === ""){
+    console.log(badEntry);
+    // if they entered Friday with a capital f OR friday with a lowercase f, prompt them to enter the temperature, store it as a number in the temp variable
+}else if(dayOfWeek === friCap || dayOfWeek === friLow){
     temp = String(prompt("What is the temperature outside (in F\xB0)?"));
     // if what the user enters is not 0 AND they don't enter anything into the prompt, or their entry is invalid, tell them to enter a valid number in the console
     if(temp != "0" && temp == false){
-        console.log("Please enter a valid number.");
+        console.log(badEntry);
      // otherwise, check if the temperature is greater than or equal to the minTemp
     }else if(temp >= minTemp){
         // if so, concatenate the string
-        goLandscape = "Its " + temp + " F\xB0 outside. I am going landscaping today!";
+        goLandscape = "Its " + dayOfWeek + " and " + temp + " F\xB0 outside. I am going landscaping today!";
         // then write it to the console
         console.log(goLandscape);
      // otherwise, print to the console that I am going to sleep in today
