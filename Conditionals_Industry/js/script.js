d3e1c45df3eaee459d041abaaf64e5b9b3d76b7a
capecodder90 <scushing90@comcast.net>
2014-10-13 17:14:37 -0400

    Setup Industry-based subproject folder
    Setup Industry-based subproject folder

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,8 @@
+/*
+Sarah A. Cushing
+Monday, October 13, 2014
+Conditionals Assignment: Industry-based
+ */
+
+// If the balance on the client’s account is paid off, I will start on their next project. Otherwise, I will send them a statement of their account.
+
