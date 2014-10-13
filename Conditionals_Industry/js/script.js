7712c1a6c68c3c562d0e5e7d38b857e8bb04a91c
capecodder90 <scushing90@comcast.net>
2014-10-13 18:40:57 -0400

    Fixed variables, added else statement
    Removed unnecessary variables, added the if statement to print the
    “good to go” message to the console if the balance is less than or
    equal to 0, and the else statement if the balance is above 0

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,20 +9,21 @@ Conditionals Assignment: Industry-based
 var balanceToStart = "0"; // declares balanceToStart, defines it as $0
 var accountBalance = prompt("What is the balance on your last statement?"); // declares accountBalance, prompts the user to input the account balance listed on their last account statement
 var accountTotal;
-var startProject; // declares startProject
+var recentPmts;
 var sendStatement; // declares sendStatement
 
-if(accountBalance == false){
-    if(accountBalance != balanceToStart){
-        console.log("Sorry, but that is not a valid number.");
-    }else if(Number(accountBalance) >= balanceToStart){
-        var recentPmts = prompt("Have you made any payments recently? If so, how much (in total)?");
-        if(recentPmts == false && recentPmts != balanceToStart){
-            console.log("Please enter a valid number.")
-        }
+if(accountBalance == false && accountBalance != balanceToStart){
+    console.log("Sorry, but that is not a valid number.");
+}else if(Number(accountBalance) >= balanceToStart){
+    recentPmts = prompt("Have you made any payments recently? If so, how much (in total)?");
+    if(recentPmts == false && recentPmts != balanceToStart){
+        console.log("Please enter a valid number.")
+    }else{
         accountTotal = accountBalance - recentPmts;
         if(accountTotal <= balanceToStart){
-            console.log("yes");
+            console.log("Your account is up-to-date. We will begin your project immediately.");
+        }else{
+            console.log("Sorry, but your account still has a balance of $" + accountTotal + ". Please remit payment at your earliest convenience, and we will begin your project upon receipt.");
         }
     }
 }
