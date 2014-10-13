1fa9bcf44f3d2162327504bf10e7c3d2d5770d6b
capecodder90 <scushing90@comcast.net>
2014-10-13 15:36:09 -0400

    Declare the givens
    Declared the first two givens for the project

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,3 +4,7 @@ Monday, October 13, 2014
 Conditionals Assignment: Personal
  */
 
+// If the weather outside is sunny, and the temperature is above 25 degrees Fahrenheit I will go landscaping. Otherwise, I will stay in my bed and sleep in.
+
+var weather = String(prompt("Is it raining, snowing, or sunny outside?"));
+var temp;
