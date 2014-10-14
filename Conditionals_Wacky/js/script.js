1ebc979969ee2edd17021e857a960dbecf5b2d58
capecodder90 <scushing90@comcast.net>
2014-10-13 20:26:35 -0400

    Created variables, wrote first if statement
    Created variables, wrote first if statement

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,5 +4,14 @@ Monday, October 13, 2014
 Conditionals Assignment: Wacky
  */
 
-// If the price of ten helium-filled balloons is $10 less than the amount of money in my wallet, I will purchase the balloons and have a wacky voice party.
+// If the price of a bunch of helium-filled balloons is $10 less than the amount of money in my wallet, I will purchase the balloons and have a wacky voice party.
 
+var oneBalloon = String(prompt("What is the price of one helium balloon?"));
+var numOfBalloons;
+var minNumBalloons = 1;
+var priceOfBalloons = oneBallo0n * 10;
+var noEntry = "That is not a valid entry.";
+
+if(oneBalloon == false && oneBallon != "0"){
+    console.log(noEntry);
+}
