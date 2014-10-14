de8115635f6868efc984407ae16ef6a3412e55f2
capecodder90 <scushing90@comcast.net>
2014-10-13 20:17:39 -0400

    Created Wacky folder and files
    Created the files and folders for the Wacky subproject

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,8 @@
+/*
+Sarah A. Cushing
+Monday, October 13, 2014
+Conditionals Assignment: Wacky
+ */
+
+// If the price of ten helium-filled balloons is $10 less than the amount of money in my wallet, I will purchase the balloons and have a wacky voice party.
+
