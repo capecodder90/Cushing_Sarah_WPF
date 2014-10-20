bf60cafdba95dc92461ae3cb51ffaeb4a741b187
capecodder90 <scushing90@comcast.net>
2014-10-20 16:39:37 -0400

    Fixed Stung & Circumference parameter and argument
    Fixed Stung & Circumference parameters and arguments

--- Functions_worksheet/js/scripts.js
+++ Functions_worksheet/js/scripts.js
@@ -10,11 +10,10 @@ Assignment: Functions Worksheet
 
 var two = 2; // declares two, defines it as 2
 var pi = 3.14; // declares pi, defines it as 3.14
-var radius = 28; // declares radius, defines it as 28
-var circumference = calcCircumference(two, pi, radius); // declares circumference, sets it equal to the value returned from the function calcCircumference with the arguments two, pi, and radius (values of which are stored in the variables)
+var circumference = calcCircumference(28); // declares circumference, sets it equal to the value returned from the function calcCircumference with the arguments two, pi, and radius (values of which are stored in the variables)
 console.log("The circumference of the circle is " + circumference + "."); // concatenate the string and print the result to the console
 
-function calcCircumference(two, pi, radius){ // creates the function calcCircumference with the parameters two, pi and radius
+function calcCircumference(radius){ // creates the function calcCircumference with the parameters two, pi and radius
     return two * pi * radius; // tells it to return the value of two times pi times radius
 }
 
@@ -23,9 +22,8 @@ function calcCircumference(two, pi, radius){ // creates the function calcCircumf
 
 // Find the number of bee stings required to kill and animal by dividing the victim's weight by 8.666666667 bee stings per pound
 
-var victimWeight = 150;
 var stingsPerPound = 8.666666667;
-var numBeeStings = calcBeeStings(victimWeight);
+var numBeeStings = calcBeeStings(140);
 console.log("It takes " + numBeeStings + " bee stings to kill this animal.");
 
 function calcBeeStings(victimWeight){
