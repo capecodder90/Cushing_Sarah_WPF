972e92677394bab2ed87531fe93a37f18052e31e
capecodder90 <scushing90@comcast.net>
2014-10-20 10:51:43 -0400

    Circumference: Add comments
    Added comments to describe what each line does

--- Functions_worksheet/js/scripts.js
+++ Functions_worksheet/js/scripts.js
@@ -8,12 +8,12 @@ Assignment: Functions Worksheet
 
 // The circumference of a circle is equal to 2 times pi times the radius
 
-var two = 2;
-var pi = 3.14;
-var radius = 28;
-var circumference = calcCircumference(two, pi, radius);
-console.log("The circumference of the circle is " + circumference + ".");
+var two = 2; // declares two, defines it as 2
+var pi = 3.14; // declares pi, defines it as 3.14
+var radius = 28; // declares radius, defines it as 28
+var circumference = calcCircumference(two, pi, radius); // declares circumference, sets it equal to the value returned from the function calcCircumference with the arguments two, pi, and radius (values of which are stored in the variables)
+console.log("The circumference of the circle is " + circumference + "."); // concatenate the string and print the result to the console
 
-function calcCircumference(two, pi, radius){
-    return two * pi * radius;
+function calcCircumference(two, pi, radius){ // creates the function calcCircumference with the parameters two, pi and radius
+    return two * pi * radius; // tells it to return the value of two times pi times radius
 }
