eac3b3c8afb8de90483b859a084156d3863fd964
capecodder90 <scushing90@comcast.net>
2014-10-20 17:25:16 -0400

    Fixed Circumference comments
    Fixed the Circumference comments to reflect the changes made earlier

--- Functions_worksheet/js/scripts.js
+++ Functions_worksheet/js/scripts.js
@@ -10,11 +10,11 @@ Assignment: Functions Worksheet
 
 var two = 2; // declares two, defines it as 2
 var pi = 3.14; // declares pi, defines it as 3.14
-var circumference = calcCircumference(28); // declares circumference, sets it equal to the value returned from the function calcCircumference with the arguments two, pi, and radius (values of which are stored in the variables)
+var circumference = calcCircumference(28); // declares circumference, sets it equal to the value returned from the function calcCircumference with the argument of radius as 28
 console.log("The circumference of the circle is " + circumference + "."); // concatenate the string and print the result to the console
 
-function calcCircumference(radius){ // creates the function calcCircumference with the parameters two, pi and radius
-    return two * pi * radius; // tells it to return the value of two times pi times radius
+function calcCircumference(radius){ // creates the function calcCircumference with the parameter of radius
+    return two * pi * radius; // tells it to return the value of two times pi times radius (set in the argument)
 }
 
 
