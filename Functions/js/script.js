b3d92dcaf85def7329245f5b4c8fbba5b2fa1f22
capecodder90 <scushing90@comcast.net>
2014-10-20 09:29:45 -0400

    Variable Scope demonstration
    Demonstrated variable scope

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,16 +1,13 @@
-// Functions - Function Execution
+// Functions - Variable Scope
 
-function outputMsg(){
-    console.log("Hello World");
-}
+var width = 5;
 
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+    //console.log(area);
 }
+calcArea();
+console.log(width);
 
-calcArea();
-calcArea();
-calcArea();
