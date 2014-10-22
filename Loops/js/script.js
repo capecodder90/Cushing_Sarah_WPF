87329fdb3fa668936585a6cf65484d62956f7229
capecodder90 <scushing90@comcast.net>
2014-10-22 09:30:52 -0400

    Loops: While Loop
    Setup and examined a while loop

---
+++ Loops/js/script.js
@@ -0,0 +1,12 @@
+// Loops
+
+// While Loop
+var b = 10; // setup the index
+
+while (b > 0){ // checks the condition
+    console.log(b + " kegs on the wall.");
+    b--; // increments or decrements the index
+}
+
+// Do While Loop
+
