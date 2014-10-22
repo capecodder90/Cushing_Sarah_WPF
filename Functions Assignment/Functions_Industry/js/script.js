64b90c4811f874fdb866fff5066dfa4415b1dfa2
capecodder90 <scushing90@comcast.net>
2014-10-22 10:57:55 -0400

    Industry: Created else statement
    Fixed NaN problem, created else statement to handle the rest of the code

--- Functions Assignment/Functions_Industry/js/script.js
+++ Functions Assignment/Functions_Industry/js/script.js
@@ -10,15 +10,22 @@ var customDesignPrice = 250;
 var pagePrice = 25;
 var numPages = prompt("How many pages does your website need?");
 var discountAmt;
-var projectPrice = calcProjectPrice(numPages, pagePrice, customDesignPrice, discountAmt);
-
 var haveDiscount = prompt("Do you have a coupon?");
-if(haveDiscount === "yes" || haveDiscount === "Yes"){
-    discountAmt = Number(prompt("What is the amount of the coupon?"));
-    console.log(projectPrice);
-}
+var projectPrice;
 
 function calcProjectPrice(numPages, pagePrice, customDesignPrice, discountAmt){
     return numPages * pagePrice + customDesignPrice - discountAmt;
 }
-//console.log("Your website build will cost you " + projectPrice + " with " + numPages + ".");
+function projectResponse(){
+    console.log("Your website build will cost you " + projectPrice + " with " + numPages + ".");
+}
+
+if(haveDiscount === "yes" || haveDiscount === "Yes"){
+    discountAmt = Number(prompt("What is the amount of the coupon?"));
+}else{
+    projectResponse();
+}
+
+projectPrice = calcProjectPrice(numPages, pagePrice, customDesignPrice, discountAmt);
+
+
