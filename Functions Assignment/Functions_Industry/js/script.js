b9b7b72ee3139338203c26ed3e39e199f37ebe01
capecodder90 <scushing90@comcast.net>
2014-10-22 10:12:17 -0400

    Industry: Setup the givens
    Setup the givens

--- Functions Assignment/Functions_Industry/js/script.js
+++ Functions Assignment/Functions_Industry/js/script.js
@@ -3,3 +3,8 @@ Sarah A. Cushing
 Wednesday, October 22, 2014
 Functions Assignment: Industry
  */
+
+// Calculate the price for a website build based on the number of pages and whether there was a discount or not.
+
+var customDesignPrice = 250;
+var pagePrice = 25;
