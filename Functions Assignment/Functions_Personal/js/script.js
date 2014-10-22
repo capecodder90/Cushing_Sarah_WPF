914d75603f9d98d7336a7553df84331be39b8f9f
capecodder90 <scushing90@comcast.net>
2014-10-22 12:43:05 -0400

    Personal: Add comments
    Added comments to describe what each line does

--- Functions Assignment/Functions_Personal/js/script.js
+++ Functions Assignment/Functions_Personal/js/script.js
@@ -6,14 +6,14 @@
 
 // How many pairs of jeans can I buy after depositing this week’s paycheck?
 
-var paycheck = Number(prompt("How much was this week's paycheck for?"));
-var inTheBank = Number(prompt("How much money in in the checking account?"));
-var priceOfJeans = 42;
-var numOfJeans = howManyJeans(paycheck, inTheBank, priceOfJeans);
+var paycheck = Number(prompt("How much was this week's paycheck for?")); // declares paycheck, prompt the user for the paycheck amount, store the value as a number
+var inTheBank = Number(prompt("How much money in in the checking account?")); // declares inTheBank, prompt the user for the amount in their checking account, store the value as a number
+var priceOfJeans = 42; // declares priceOfJeans, defines it as $42
+var numOfJeans = howManyJeans(paycheck, inTheBank, priceOfJeans); // declares numOfJeans, sets it equal to the value returned from the function howManyJeans
 
+// creates howManyJeans function with the parameters paycheck, inTheBank and priceOfJeans
 function howManyJeans(paycheck, inTheBank, priceOfJeans){
-    return parseInt((paycheck + inTheBank) / priceOfJeans);
+    return parseInt((paycheck + inTheBank) / priceOfJeans); // calculates the number of jeans that can be purchased and returns the value
 }
 
-console.log("I can buy " + numOfJeans + " pairs of jeans.");
-console.log(paycheck + inTheBank);
+console.log("I can buy " + numOfJeans + " pairs of jeans."); // concatenated the string, prints it to the console
