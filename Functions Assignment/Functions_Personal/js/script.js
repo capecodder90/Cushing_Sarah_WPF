ab66b990010bf40a506c357f0dca4b5e8f6b5ffd
capecodder90 <scushing90@comcast.net>
2014-10-22 12:31:38 -0400

    Personal: Declare variables
    Declared the variables for the problem

--- Functions Assignment/Functions_Personal/js/script.js
+++ Functions Assignment/Functions_Personal/js/script.js
@@ -3,3 +3,10 @@
  Wednesday, October 22, 2014
  Functions Assignment: Personal
  */
+
+// How many pairs of jeans can I buy after depositing this week’s paycheck?
+
+var paycheck = prompt("How much was this week's paycheck for?");
+var inTheBank = prompt("How much money in in the checking account?");
+var priceOfJeans = 42;
+var numOfJeans = howManyJeans(paycheck, inTheBank);
