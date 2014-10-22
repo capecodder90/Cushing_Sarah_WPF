5fbe09d500a32cd2c910e369db556852e4a702f7
capecodder90 <scushing90@comcast.net>
2014-10-22 13:21:13 -0400

    Wacky: Setup variables
    Setup variables for the problem

--- Functions Assignment/Functions_Wacky/js/script.js
+++ Functions Assignment/Functions_Wacky/js/script.js
@@ -3,3 +3,9 @@
  Wednesday, October 22, 2014
  Functions Assignment: Wacky
  */
+
+// If you are only allowed 5 orange sprinkles per cupcake, how many cupcakes will you need to eat before you eat all the orange sprinkles that you want?
+
+var orangeSprinkles = 5;
+var sprinkles = Number(prompt("How many sprinkles do you need to eat?"));
+var cupcakesToEat = Number(prompt("How many cupcakes do you think you can eat right now?"));
